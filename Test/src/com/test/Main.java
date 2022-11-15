aede4aac7139387619c1d246fe5511522316fc4d
Stojanche Gigov <85480530+stojanche-gigov@users.noreply.github.com>
2022-11-15 16:45:32 +0100

    comit3

--- Test/src/com/test/Main.java
+++ Test/src/com/test/Main.java
@@ -3,7 +3,7 @@ package com.test;
 public class Main {
 
 	public static void main(String[] args) {
-		String test="test2";
+		String test="test22";
 		// TODO Auto-generated method stub
 //		Comment1
 	}
